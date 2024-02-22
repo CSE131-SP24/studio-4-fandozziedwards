d2a096d68cef61bd1a1407d9af69d99a0e3fdd2c
edwardsr <edwardsr@1V000WL-9VJ02Z2.accounts.ad.wustl.edu>
2024-02-22 12:33:21 -0600

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,15 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.clear();
+		StdDraw.setPenColor(227, 5, 5);
+		StdDraw.filledRectangle(.5, .5, .5, .25);
+		StdDraw.setPenColor(5, 97, 29);
+		StdDraw.filledEllipse(.5, .5, .15, .15);
+		StdDraw.setPenRadius(.05);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.rectangle(.5, .5, .5, .25);
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,61 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		double parameterFive = in.nextDouble();
+		double parameterSix = in.nextDouble();
 		
+
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+
+		if(shapeType.equals("rectangle")) {
+
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+
+		else if(shapeType.equals("ellipse")) {
+
+			if (isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		
+		else if(shapeType.equals("triangle")) {
+			
+			double [] x = {parameterOne, parameterThree, parameterFive};
+			double [] y = {parameterTwo, parameterFour, parameterSix};
+			
+			if (isFilled == true) {
+				StdDraw.filledPolygon(x, y);
+				
+			}else {
+				StdDraw.polygon(x, y);
+			}
+		}
+		/*StdDraw.clear();
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);*/
+
 	}
 }
